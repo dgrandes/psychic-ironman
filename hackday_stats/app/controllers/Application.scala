31ca91999377019cfb4d7b9841ab776582933e78
David Grandes <dgrandes@gmail.com>
2012-05-12 16:22:05 -0300

    Hecho el form inicial

--- hackday_stats/app/controllers/Application.scala
+++ hackday_stats/app/controllers/Application.scala
@@ -2,15 +2,25 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
+import views._
 object Application extends Controller {
   
+  val searchForm = Form(
+		  "item"->nonEmptyText
+      )
+      
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(searchForm))
   }
   
-  def searchItem = Action {
-    Ok("Searching Meli")
+  def searchItem = Action { 
+    implicit request => searchForm.bindFromRequest.fold(
+        hasErrors => Ok(views.html.index(searchForm)),
+        {case(item) => Ok("Searching MELI for "+item)}
+        )
   }
   
 }
