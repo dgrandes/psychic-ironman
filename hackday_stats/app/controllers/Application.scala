6105877292550a9ecd201e83555657972769bef7
Xin W. Gou <gouxinwei@gmail.com>
2012-05-12 17:37:59 -0300

    Added search by required keyword

--- hackday_stats/app/controllers/Application.scala
+++ hackday_stats/app/controllers/Application.scala
@@ -5,9 +5,14 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 import views._
+
 object Application extends Controller {
-  
   val searchForm = Form(
 		  "item"->nonEmptyText
       )
@@ -19,8 +24,16 @@ object Application extends Controller {
   def searchItem = Action { 
     implicit request => searchForm.bindFromRequest.fold(
         hasErrors => Ok(views.html.index(searchForm)),
-        {case(item) => Ok("Searching MELI for "+item)}
+        {case(item) => Ok(search(item))}
         )
   }
   
+  def search (item:String):String = {
+    val url = "https://api.mercadolibre.com/sites/MLA/search?q=" + item
+	val httpClient = new DefaultHttpClient()
+	val httpget = new HttpGet(url)
+	val responseHandler = new BasicResponseHandler()
+	val responseBody = httpClient.execute(httpget, responseHandler)
+	return responseBody
+  }
 }
