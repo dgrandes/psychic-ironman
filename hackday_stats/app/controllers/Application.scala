c0963f73b222bfddd86ee5fe1cd0c1b367c1c03f
Gonzalo M. Rey <cliksmodernos@gmail.com>
2012-05-13 02:13:36 -0300

    Added the magic query filter

--- hackday_stats/app/controllers/Application.scala
+++ hackday_stats/app/controllers/Application.scala
@@ -5,48 +5,65 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import models._
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.ResponseHandler
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.BasicResponseHandler
+import org.apache.http.impl.client.DefaultHttpClient
 import views._
 
 object Application extends Controller {
   val searchForm = Form(
-    "item" -> nonEmptyText)
+    "q" -> nonEmptyText)
 
   def index = Action {
     Ok(views.html.index(searchForm))
   }
 
-  def searchItem = Action {
+  def search = Action {
     implicit request =>
       searchForm.bindFromRequest.fold(
         hasErrors => Ok(views.html.index(searchForm)),
-        { 
-          
-          case (item) => 
-            val parser = new JSONParser(search(item))
-            val categories_list = processResult(parser.getFilters())
-            Ok(views.html.result(categories_list, "aaaaaaa"))
-          })
+        {
+
+          case (q) =>
+            val query = "q=" + q
+            val parser = new JSONParser(searchToMELI(query))
+            val filters = processResult(parser.getFilters())
+            Ok(views.html.result(parser.getFilters(), query))
+        })
   }
 
-  def processResult(categories: List[Filters]): List[Item] = {
-   // categories.flatMap(_._2.values.map(k =>))
+  def process = Action {
+    implicit request =>
+      val query = buildQuery(request.queryString)
+      val parser = new JSONParser(searchToMELI(query))
+      val filters = processResult(parser.getFilters())
+      Ok(views.html.result(parser.getFilters(), query))
+  }
+
+  def processResult(filters: List[Filters]): List[Filters] = {
+     // categories.flatMap(_._2.values.map(k =>))
     //Item(id: String, title: String, price: Double, sold_quantity: Int, permalink: String, thumbnail: String)
-    List(Item("MLAItem1", "Titulo1", 12.01, 10, "http://disney.com", "http://www.bdigital.unal.edu.co/style/images/fileicons/text_html.png"),
-        Item("MLAItem2", "Titulo2", 13.01, 11, "http://disney.com", "http://www.bdigital.unal.edu.co/style/images/fileicons/text_html.png"),
-        Item("MLAItem3", "Titulo3", 14.01, 12, "http://disney.com", "http://www.bdigital.unal.edu.co/style/images/fileicons/text_html.png"))
+    //List(Item("MLAItem1", "Titulo1", 12.01, 10, "http://disney.com", "http://www.bdigital.unal.edu.co/style/images/fileicons/text_html.png"),
+    //Item("MLAItem2", "Titulo2", 13.01, 11, "http://disney.com", "http://www.bdigital.unal.edu.co/style/images/fileicons/text_html.png"),
+    //Item("MLAItem3", "Titulo3", 14.01, 12, "http://disney.com", "http://www.bdigital.unal.edu.co/style/images/fileicons/text_html.png"))
+    filters
   }
 
-  def search(item: String): String = {
-    val url = "https://api.mercadolibre.com/sites/MLA/search?q=" + item
+  def buildQuery(queryMap: Map[String, Seq[String]]): String = {
+    queryMap.keys.toList.sortWith(_.length < _.length).map(f => f + "=" + queryMap(f)(0) + "&").foldLeft("")((a, b) => a + b).dropRight(1)
+  }
+
+  def searchToMELI(query: String): String = {
+    val url = "https://api.mercadolibre.com/sites/MLA/search?" + query
+    httpRequest(url)
+  }
+
+  def httpRequest(url: String): String = {
     val httpClient = new DefaultHttpClient()
     val httpget = new HttpGet(url)
     val responseHandler = new BasicResponseHandler()
-    val responseBody = httpClient.execute(httpget, responseHandler)
-    return responseBody
+    httpClient.execute(httpget, responseHandler)
   }
 }
